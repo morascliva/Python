bb29969da81f8973bdefb313f85cea610909a66f
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-09 01:14:21 +0530

    Create faces.py

---
+++ faces.py
@@ -0,0 +1,11 @@
+
+def convert(text):
+
+    return text.replace(":)", "🙂").replace(":(", "🙁")
+
+def main():
+    user_input = input("Enter a message: ")
+    print(convert(user_input))
+
+
+main()
