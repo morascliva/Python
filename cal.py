1e2a02ada882528a737939ad8d8c57b20f49e565
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-08 22:04:51 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,24 @@
+#x= input("Whats x")
+#y= input("Whats y")
+#z=x+y
+# output= xy
+
+#z= int(x) +int (y)
+#output= x+y
+#print(z)
+
+x= float(input("Whats x"))
+y= float(input("Whats y"))
+
+#print(float(x+y))
+#z=round(x+y)
+#print(z)
+#print(f"{z:,}")
+#output with commas
+
+
+#z=round(x/y)
+#print(z)
+
+z=x/y
+print(f"{z:.2f}")
