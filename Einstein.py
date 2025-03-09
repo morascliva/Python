948eb885c039487feb3452e994fab4aabcd4a1cb
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-09 19:26:39 +0530

    Create Einstein.py

---
+++ Einstein.py
@@ -0,0 +1,7 @@
+def mass_to_energy(mass):
+    c = 300000000 
+    energy = mass * c**2
+    return energy
+
+mass = int(input("Mass in kg: "))  
+print(f"Energy (J): {mass_to_energy(mass)}")  # Output energy in Joules
