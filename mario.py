4823719c0f0c1d2150f3abe227fba97f282e552e
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-11 01:10:11 +0530

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,57 @@
+#print("#")
+#print("#")
+#print("#")
+
+#for _ in range(3):
+#    print("#")
+
+
+#vertical
+#def main():
+#    print_col(3)
+#
+#def print_col(height):
+#    print("#\n"*height,end="")
+
+#main()
+
+
+#horizontal
+#def main():
+#    print_row(4)
+
+#def print_row(width):
+#    print("?"* width)
+
+#main()   
+
+
+def main():
+    print_square(3)
+
+def  print_square(size):
+
+    #for each row in sqaure
+    for i in range(size):
+
+    #for each brick in row
+        for j in range(size):
+
+
+     #print brick
+         print("#",end="")
+        print()
+
+
+main()            
+
+
+#def main():
+#   print_square(3)
+
+#def  print_square(size):
+#for i in range(size):
+#   print("#"*size)
+
+
+#main()   
