5d8d0b7121d411c3111c695b1c4186bf4d29d567
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-10 23:49:52 +0530

    Create forcat.py

---
+++ forcat.py
@@ -0,0 +1,35 @@
+#for i in [0,1,2]:
+#    print("meow")
+
+#for i in range(3):
+#    print("meow")
+
+
+#print("meow\n" *3, end="")    
+
+
+
+#while True:
+#    n = int(input("Whats n?"))
+#    if n>0:
+#        break
+#for _ in range(n):
+#    print("meow")    
+
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n=int(input("whats n"))    
+        if n>0:
+            break
+    return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow")    
+
+main()        
