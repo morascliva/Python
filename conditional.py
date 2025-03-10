49ece14aedb5466fae310968fbfa431d4c46f2da
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-10 09:58:43 +0530

    Create conditional.py

---
+++ conditional.py
@@ -0,0 +1,44 @@
+#x= int(input("Whats x?"))
+#y=int(input("Whats y?"))
+
+#if x<y:
+#    print("x is less than y")
+
+#if y<x:
+#    print("y is less than x")
+
+#if x==y:
+#    print("x is equal to y")        
+
+#x= int(input("Whats x?"))
+#y=int(input("Whats y?"))
+
+#if x<y:
+#    print("x is less than y")
+
+#elif y<x:
+#    print("y is less than x")
+
+#elif x==y:
+#    print("x is equal to y")   
+
+# elif short for else if ,if it finds a condition true it skips the rest 
+#whereas if run each and every condition
+
+
+#x= int(input("Whats x?"))
+#y= int(input("Whats y?"))
+
+#if x<y or x>y:
+#    print("x is not equal to y")
+#else:
+#    print("x is equal to y")    
+
+
+x= int(input("Whats x?"))
+y= int(input("Whats y?"))
+
+if x!=y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y") 
