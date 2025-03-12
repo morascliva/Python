a599a50f3c0e5c5a62aa0e33c444f2aac5a8aa3a
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-12 14:23:55 +0530

    Update name.py

--- libraries/name.py
+++ libraries/name.py
@@ -11,10 +11,33 @@
 #  print("Too few arguments")
 
 
-import sys
+#import sys
+#if len(sys.argv)<2:
+#  print("Too few arguments")
+#elif len(sys.argv)>2  
+#  print("Too many arguments")
+#else:
+#  print("hello,my name is",sys.argv[1])
+
+#import sys 
+# check for errors
+#if len(sys.argv)<2:
+#   sys.exit("Too few arguments")
+#elif len(sys.argv)>2 
+#   sys.exit("Too many arguments")
+
+#print("Hello, my name is",sys.argv[1])
+
+
+#slice
+import sys 
 if len(sys.argv)<2:
-  print("Too few arguments")
-elif len(sys.argv)>2  
-  print("Too many arguments")
-else:
-  print("hello,my name is",sys.argv[1])
+   sys.exit("Too few arguments")
+for arg in sys.argv[1:]:
+print("Hello, my name is",arg)
+
+
+
+
+
+#print name tags
