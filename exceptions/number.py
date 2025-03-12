c0588f181eb4e6b2da9a990b8109c1a535492d10
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-12 11:32:12 +0530

    Update number.py

--- exceptions/number.py
+++ exceptions/number.py
@@ -13,13 +13,45 @@
 #  print(f"x is {x}")
 
 
-while True:
-    try:
-      x = int(input("Whats x?"))
-    except ValueError:
-      print("x is not an integer")
-    else:
-      break
-print(f"x is {x}")      
+#while True:
+#    try:
+#      x = int(input("Whats x?"))
+#    except ValueError:
+#      print("x is not an integer")
+#    else:
+#      break
+#print(f"x is {x}")      
 
 
+
+
+#get main():
+# x=get_int()
+#print(f"x is {x}")
+
+
+# def get_int():   
+#    while True:
+#        try:
+#          x = int(input("Whats x?"))
+#        except ValueError:
+#          print("x is not an integer")
+#        else:
+#          return x
+
+#main()
+
+
+get main():
+ x=get_int()
+print(f"x is {x}")
+
+
+ def get_int():   
+    while True:
+        try:
+          return x = int(input("Whats x?"))
+        except ValueError:
+          pass
+
+main()
