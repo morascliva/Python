6f2137a0da3c01ce01ca0952a2e6d303f191d411
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-12 12:01:30 +0530

    Create gernerate.py

---
+++ libraries/gernerate.py
@@ -0,0 +1,18 @@
+#import random
+#coin = random.choice(["heads","tails"])
+#print(coin)
+
+
+#from random import choice 
+#coin = choice(["heads","tails"])
+#print(coin)
+
+#import random
+#number= random.randint(1,10)
+#print(number)
+
+import random
+cards =(["jack","queen","king"])
+random.shuffle(cards)
+for card in cards:
+  print(card)
