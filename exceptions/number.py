c8ccafd9a308b82369d26f6617647c2faa1ba076
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-12 11:19:57 +0530

    Update number.py

--- exceptions/number.py
+++ exceptions/number.py
@@ -1,5 +1,25 @@
-try:
-  x=int(input("Whats x?"))
-  print(f"x is {x}")
-except ValueError:
-    print("x is not an integer")   
+#try:
+#  x=int(input("Whats x?"))
+#  print(f"x is {x}")
+#except ValueError:
+#    print("x is not an integer")
+
+
+#try:
+#  x = int(input("Whats x?"))
+#except ValueError:
+#  print("x is not an integer")
+#else:
+#  print(f"x is {x}")
+
+
+while True:
+    try:
+      x = int(input("Whats x?"))
+    except ValueError:
+      print("x is not an integer")
+    else:
+      break
+print(f"x is {x}")      
+
+
