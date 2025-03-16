852bccfb05a57604ce6b7753b0a7c7560adee81b
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-16 23:41:46 +0530

    Update 4.whilecat.py

--- 4.whilecat.py
+++ 4.whilecat.py
@@ -17,3 +17,43 @@ i=0
 while i<3:
     print("meow")
     i+=1
+
+
+ for i in [0, 1, 2]: 
+     print("meow")
+
+for i in range(3): 
+    print("meow")
+
+
+print("meow\n" * 3, end="")
+
+
+
+---------------------------------------
+while True:
+n= int(input("What's n? "))
+ if n <=0:
+     continue
+  else:
+     break
+for_ in range(n):
+   print("meow")
+--------------------------------------------
+
+
+
+def main()
+  meow(get_number)
+
+def get_number():
+    while True:
+        n=int(input("Whats n?")
+        if n>1:
+              return n 
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
