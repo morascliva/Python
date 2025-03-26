f9b05d6d67a70a34e7a2c7123d69483a15b63b4f
Cliva Moras <94843082+morascliva@users.noreply.github.com>
2025-03-26 16:49:17 +0530

    Create intepreter.py

---
+++ intepreter.py
@@ -0,0 +1,23 @@
+def main():
+    expression = input("Expression: ").strip()
+    
+    x, operator, z = expression.split()
+    x = int(x)
+    z = int(z)
+    
+    if operator == "+":
+        result = x + z
+    elif operator == "-":
+        result = x - z
+    elif operator == "*":
+        result = x * z
+    elif operator == "/":
+        result = x / z
+    else:
+        print("Invalid operator")
+        return
+    
+    print(f"{result:.1f}")
+
+
+    main()
